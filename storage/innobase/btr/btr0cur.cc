e843dc3b40c140855be3c258d2ff0a18a2d93a29
unconsolable <chenzhipeng2012@gmail.com>
2026-05-26 09:27:12 +0000

    disable another in node linear model

--- storage/innobase/btr/btr0cur.cc
+++ storage/innobase/btr/btr0cur.cc
@@ -1491,15 +1491,16 @@ retry_page_get:
     if (found && page_mode == PAGE_CUR_RTREE_GET_FATHER) {
       cursor->low_match = DICT_INDEX_SPATIAL_NODEPTR_SIZE + 1;
     }
-  } else if (height == 0 && btr_search_enabled &&
-             !dict_index_is_spatial(index)) {
-    /* The adaptive hash index is only used when searching
-    for leaf pages (height==0), but not in r-trees.
-    We only need the byte prefix comparison for the purpose
-    of updating the adaptive hash index. */
-    page_cur_search_with_match_bytes(block, index, tuple, page_mode, &up_match,
-                                     &up_bytes, &low_match, &low_bytes,
-                                     page_cursor);
+  // AHI is replaced by Shadow, this version only uses shadow and disables intra-leaf model.
+  // } else if (height == 0 && btr_search_enabled &&
+  //            !dict_index_is_spatial(index)) {
+  //   /* The adaptive hash index is only used when searching
+  //   for leaf pages (height==0), but not in r-trees.
+  //   We only need the byte prefix comparison for the purpose
+  //   of updating the adaptive hash index. */
+  //   page_cur_search_with_match_bytes(block, index, tuple, page_mode, &up_match,
+  //                                    &up_bytes, &low_match, &low_bytes,
+  //                                    page_cursor);
   } else {
     /* Search for complete index fields. */
     up_bytes = low_bytes = 0;
